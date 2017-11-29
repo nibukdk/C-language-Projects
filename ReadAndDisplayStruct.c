c64403da94a02a0007bbbe26ca399af5a2c2c107
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-11-29 15:44:25 +0200

    Data format

--- ReadAndDisplayStruct.c
+++ ReadAndDisplayStruct.c
@@ -17,9 +17,8 @@ int main(){
 			printf("Error in appending file.");
 		}
 		else {
-
     			for(int i=0; i<strlen(readFile); i++){
-					fscanf(readFile,"%ds",&contactList[i].num);
+								fscanf(readFile,"%ds",&contactList[i].num);
           			fscanf(readFile,"%d %s %s %s",&contactList[i].num,&contactList[i].firstName[0],&contactList[i].lastName[0],&contactList[i].telphoneNum[0]);
           			fscanf(readFile,"%s %s %s",&contactList[i].firstName[0],&contactList[i].lastName[0],&contactList[i].telphoneNum[0]);
           			printf("%s %s %s\n",contactList[i].firstName,contactList[i].lastName,contactList[i].telphoneNum);
