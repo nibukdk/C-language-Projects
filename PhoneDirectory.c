9ed95db5858fa0951551671aeeb0024c166da9a8
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-11-29 14:34:08 +0200

    Phone direcotry using struct

---
+++ PhoneDirectory.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+	char fileName[]="phonedir.txt";
+	FILE *appendFile=fopen(fileName,"a");
+	struct PhoneDir{
+		char firstName[21];
+		char lastName[21];
+		char telphoneNum[21];
+
+	};
+
+	struct PhoneDir contactList;
+	
+
+		printf("Enter first name:");
+		scanf("%s",&contactList.firstName[0]);
+		printf("Enter last name:");
+		scanf("%s",&contactList.lastName[0]);
+		printf("Enter telephone number:");
+		scanf("%s",&contactList.telphoneNum[0]);
+
+		if(appendFile==NULL){
+			printf("Error in appending file.");
+		}
+		else{
+			fprintf(appendFile,"%s %s %s",&contactList.firstName[0],&contactList.lastName[0],&contactList.telphoneNum[0]);
+
+		}
+		printf("Successfully saved the data.");
+
+
+	fclose(appendFile);
+
+}
